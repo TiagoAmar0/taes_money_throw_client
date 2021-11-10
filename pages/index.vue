2d33394be3e6637afadc227e1b39878a7a43c67c
tiago <tiago.amaro.jorge@gmail.com>
2021-11-10 10:34:52 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="height:100vh" class="container is-max-widescreen is-flex is-flex-direction-column is-justify-content-flex-end">
+    <footer class="footer">
+      <nuxt-link to="/register">
+        <b-button type="is-success" expanded outlined>Criar Conta</b-button>
+      </nuxt-link>
+      <nuxt-link to="/login">
+        <b-button type="is-success mt-1" expanded>Iniciar Sessão</b-button>
+      </nuxt-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LandPage'
+  }
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="height: 100vh" class="container is-max-widescreen is-flex is-flex-direction-column is-justify-content-space-between">
+    <section class="section">
+      <h2 class="title is-4 has-text-justified">Iniciar Sessão no Money Throw</h2>
+      <p class="subtitle is-6 has-text-justified">Para inicar sessão, introduza o seu número de telemóvel registado</p>
+
+      <div class="field">
+        <label class="label">Nº de Telemóvel</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Nº de Telemóvel" />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="Password" />
+        </div>
+      </div>
+    </section>
+    <footer class="footer">
+      <nuxt-link to="/login">
+        <b-button type="is-success mb-1" expanded>Iniciar Sessão</b-button>
+      </nuxt-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
